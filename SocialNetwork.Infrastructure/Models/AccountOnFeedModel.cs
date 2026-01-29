83044990ba14a00d9bf2c423e6ce4cb79dd0d7ba
minhdnhe163670 <minhdnhe163670@fpt.edu.vn>
2026-01-30 02:18:40 +0700

    fix: View Newfeed function

---
+++ SocialNetwork.Infrastructure/Models/AccountOnFeedModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Infrastructure.Models
+{
+    public class AccountOnFeedModel
+    {
+        public Guid AccountId { get; set; }
+        public string Username { get; set; } = null!;
+        public string FullName { get; set; } = null!;
+        public string? AvatarUrl { get; set; }
+        public bool IsFollowedByCurrentUser { get; set; } = false;
+    }
+}
