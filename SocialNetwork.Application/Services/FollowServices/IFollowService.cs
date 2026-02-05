c21af1f92f0a7de5e24ebb5c73cbb78545968ec1
minhdnhe163670 <minhdnhe163670@fpt.edu.vn>
2026-02-06 05:09:12 +0700

    fix: update post mapping and userhub logic

--- SocialNetwork.Application/Services/FollowServices/IFollowService.cs
+++ SocialNetwork.Application/Services/FollowServices/IFollowService.cs
@@ -16,5 +16,6 @@ namespace SocialNetwork.Application.Services.FollowServices
         Task<bool> IsFollowingAsync(Guid followerId, Guid targetId);
         Task<PagedResponse<AccountWithFollowStatusModel>> GetFollowersAsync(Guid userId, Guid? currentId, FollowPagingRequest request);
         Task<PagedResponse<AccountWithFollowStatusModel>> GetFollowingAsync(Guid userId, Guid? currentId, FollowPagingRequest request);
+        Task<FollowCountResponse> GetStatsAsync(Guid userId);
     }
 }
