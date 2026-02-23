53504aed368d67acd89868b7341319431075e581
minhdnhe163670 <minhdnhe163670@fpt.edu.vn>
2026-02-24 01:45:37 +0700

    feat(story): add mechanism to display story status on avatar newfeed and adjust the Stories table

--- SocialNetwork.Application/Services/StoryServices/IStoryService.cs
+++ SocialNetwork.Application/Services/StoryServices/IStoryService.cs
@@ -1,4 +1,5 @@
 using SocialNetwork.Application.DTOs.StoryDTOs;
+using SocialNetwork.Domain.Enums;
 
 namespace SocialNetwork.Application.Services.StoryServices
 {
@@ -7,5 +8,6 @@ namespace SocialNetwork.Application.Services.StoryServices
         Task<StoryDetailResponse> CreateStoryAsync(Guid currentId, StoryCreateRequest request);
         Task<StoryDetailResponse> UpdateStoryPrivacyAsync(Guid storyId, Guid currentId, StoryPrivacyUpdateRequest request);
         Task SoftDeleteStoryAsync(Guid storyId, Guid currentId);
+        Task<IReadOnlyDictionary<Guid, StoryRingStateEnum>> GetStoryRingStatesForAuthorsAsync(Guid currentId, IEnumerable<Guid> authorIds);
     }
 }
